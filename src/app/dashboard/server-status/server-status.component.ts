91ca332c12fca562c14aec91d361fdc9b37a6b6e
Sneyder Ospina Chavarria <sneyder.ospina@e-hps.com>
2025-02-17 15:30:23 -0500

    Submission repetition and Lifecycle methods

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit, OnDestroy {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  //private interval?: NodeJS.Timeout;
+  private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
+
+  constructor(){}
+
+  ngOnInit(){
+    //this.interval = setInterval(()=>{
+      const interval = setInterval(()=>{
+      const rnd = Math.random();
+
+      if(rnd < 0.5){
+        this.currentStatus = 'online';
+      } else if(rnd < 0.9){
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
+  }
+
+  ngAfterViewInit(){
+    console.log('Init');
+  }
+
+  ngOnDestroy(): void {
+      clearTimeout(this.interval);
+  }
 }
